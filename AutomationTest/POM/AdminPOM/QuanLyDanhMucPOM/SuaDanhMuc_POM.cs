ee1b2ee439904042beb9d13071bdc054e62f146b
cochanh <quynhtrang1102.epu@gmail.com>
2023-01-15 17:21:52 +0700

    add first automation test

---
+++ AutomationTest/POM/AdminPOM/QuanLyDanhMucPOM/SuaDanhMuc_POM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTest.POM.AdminPOM.QuanLyDanhMucPOM
+{
+    internal class SuaDanhMuc_POM
+    {
+    }
+}
